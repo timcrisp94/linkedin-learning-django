060206b72dbca2b42ea1db1685823e24d4074527
Tim Crisp <timothycrisp@gmail.com>
2023-04-12 15:47:13 -0500

    add css

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,18 @@
+from typing import List
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
 
 def detail(request, pk):
     note = Notes.objects.get(pk=pk)
